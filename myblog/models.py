217fb72f3b60409e9d55019d1b0a32f07f632843
pphilippersson6 <>
2023-08-28 18:04:36 +0000

    added my first post for my blog

--- myblog/models.py
+++ myblog/models.py
@@ -3,12 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-
-
     title = models.CharField(max_length=225)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
-
-def __str__(self):
-    return self.title + ' I ' + self.author
+    def __str__(self):
+        return self.title + ' I ' + str(self.author)
