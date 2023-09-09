fcadfb94bfb473575055bcfd1869dc2f56962fcb
pphilippersson6 <>
2023-09-09 14:07:11 +0000

    added userid, so only the correct user can edit/delete posts

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,11 +5,11 @@ from .views import HomeView, ArticleDetailView, AddPostView, EditPostView, Delet
 urlpatterns = [
     # path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
     path('add_blogpost/', AddPostView.as_view(), name= 'add_post'),
     path('add_category/', AddCategoryView.as_view(), name= 'add_category'),
-    path('article/edit/<int:pk>', EditPostView.as_view(), name='edit_post'),
-    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
-    path('category/<str:categ>', CategoryView, name='category'),
+    path('article/edit/<int:pk>/', EditPostView.as_view(), name='edit_post'),
+    path('article/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:categ>/', CategoryView, name='category'),
     path('category-list/', CategoryListView, name='category-list'),
 ]
