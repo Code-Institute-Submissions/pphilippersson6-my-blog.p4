e18e5e188346b8a571fe13015371be9b820ac8de
pphilippersson6 <>
2023-09-13 08:58:37 +0000

    created an edit profile page

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,6 @@ from .views import UserRegisterView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 
 ]
