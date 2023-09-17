135b8dd625a9eaa9fea671293a0a41ae0ac374d6
pphilippersson6 <>
2023-09-17 19:49:46 +0000

    created a dropdown list in navbar and also hte edit page urls

--- members/views.py
+++ members/views.py
@@ -7,14 +7,19 @@ from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 from myblog.models import Profile
 
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url','linkedin_url']
+    success_url = reverse_lazy('home')
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        #users = Profile.objects.all()
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
-        
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
 
         context["page_user"] = page_user
